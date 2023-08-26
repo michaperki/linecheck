d1301e9358d943e0715bdbc3010789d57d2d4b35
Michael Perkins <mperkins1995@gmail.com>
2023-08-26 04:31:08 -0400

    a

--- backend/app/modules/video_processing.py
+++ backend/app/modules/video_processing.py
@@ -1,8 +1,8 @@
-# app/modules/video_processing.py
 import cv2
 import os
 import numpy as np
-from app import config
+from app import app
+from app.config import Config  # Import the Config class
 
 def process_video(video_path):
     # break the video into frames and save them
@@ -13,7 +13,7 @@ def process_video(video_path):
     count = 0
     success = True
     while success:
-        cv2.imwrite(os.path.join(config.PROCESSED_FOLDER, "frame%d.jpg" % count), image)     # save frame as JPEG file      
+        cv2.imwrite(os.path.join(Config.PROCESSED_FOLDER, "frame%d.jpg" % count), image)     # save frame as JPEG file      
         success,image = vidcap.read()
         count += 1
-    return os.path.join(config.PROCESSED_FOLDER, "frame0.jpg")
+    return os.path.join(Config.PROCESSED_FOLDER, "frame0.jpg")
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,9 @@
 from app import app
 from flask_cors import CORS
 
+# Load configuration
+app.config.from_object('app.config.Config')
+
 # Enable CORS for all routes
 CORS(app)
 
